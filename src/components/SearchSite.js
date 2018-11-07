a260a0b597fd42e018c549e333d37a3b957dc31e
trmatt <39041501+trmatt@users.noreply.github.com>
2018-11-06 21:33:44 -0600

    Move search functionality from App.js to SearchSite.js

--- src/components/SearchSite.js
+++ src/components/SearchSite.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 class SearchSite extends React.Component {
     render() {
         return (
-            <p>Search Site</p>
+            <div className="search-books">
+                <div className="search-books-bar">
+                  <Link className="close-search" to="/">Close</Link>
+                  <div className="search-books-input-wrapper">
+                    <input type="text" placeholder="Search by title or author"/>
+    
+                  </div>
+                </div>
+                <div className="search-books-results">
+                  <ol className="books-grid"></ol>
+                </div>
+            </div>
         );
     }
 }
