bc96d5a66a2561e3765c2e444cc7f9578d9be336
trmatt <39041501+trmatt@users.noreply.github.com>
2018-11-06 21:03:20 -0600

    Modify App.js: include Route component; index.js: include BrowserRouter component

--- src/App.js
+++ src/App.js
@@ -2,18 +2,23 @@ import React from 'react'
 // import * as BooksAPI from './BooksAPI'
 import './App.css'
 
-class BooksApp extends React.Component {
-  state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
-  }
+import { Route } from 'react-router-dom'
 
+import MainSite from './components/MainSite'
+
+class BooksApp extends React.Component {
   render() {
+    return (
+      <div>
+        <Route exact path="/" component={ MainSite } />
+      </div>
+
+    );
+    
+
+
+  
+
     return (
       <div className="app">
         {this.state.showSearchPage ? (
