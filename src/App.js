4dc7a82f7347ea86cc029e431117a7c513aa0449
trmatt <39041501+trmatt@users.noreply.github.com>
2018-11-06 21:19:10 -0600

    Create the SearchSite component

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import './App.css'
 import { Route } from 'react-router-dom'
 
 import MainSite from './components/MainSite'
+import SearchSite from './components/SearchSite'
 
 class BooksApp extends React.Component {
   render() {
     return (
       <div>
         <Route exact path="/" component={ MainSite } />
+        <Route exact path="/search" component={ SearchSite } />
       </div>
 
     );
---
+++ src/components/SearchSite.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class SearchSite extends React.Component {
+    render() {
+        return (
+            <p>Search Site</p>
+        );
+    }
+}
+
+export default SearchSite
